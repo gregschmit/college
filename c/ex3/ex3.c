f107add24c1ed2cd55c821ab9dc3228071b85f9b
Gregory N. Schmit <schmitgreg@outlook.com>
2016-01-16 21:36:27 -0600

    gns

---
+++ c/ex3/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 24;
+    int weight = 170;
+    
+    printf("I am %d years old.\n",age);
+    printf("I weigh %d pounds.\n",weight);
+
+    return 0;
+}
