1859f5bdff6cf709be6f455c99a657f148ab65b2
Gregory N. Schmit <schmitgreg@outlook.com>
2016-01-17 14:26:09 -0600

    ex4

---
+++ c/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Warning: this C file is wrong on purpose.
+
+int main()
+{
+    int age = 10;
+    int height;
+
+    printf("I am %d years old./n");
+    printf("I am %d inches tall./n", height);
+
+    return 0;
+}
