338ab7267b54bb5f8e633f4bf4635cbfe9075662
Gregory N. Schmit <schmitgreg@gmail.com>
2024-04-12 15:43:25 -0500

    Fix to remove loop and add period.

--- coursera/algo1/hw1/HelloGoodbye.java
+++ coursera/algo1/hw1/HelloGoodbye.java
@@ -1,16 +1,6 @@
 public class HelloGoodbye {
     public static void main(String[] args) {
-        System.out.println("Hello " + String.join(" and ", args));
-
-        // Say goodbye and then print the arguments in reverse order, separated by
-        // "and".
-        System.out.print("Goodbye ");
-        for (int i = args.length - 1; i >= 0; i--) {
-            System.out.print(args[i]);
-            if (i > 0) {
-                System.out.print(" and ");
-            }
-        }
-        System.out.println();
+        System.out.println("Hello " + String.join(" and ", args) + ".");
+        System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
     }
 }
